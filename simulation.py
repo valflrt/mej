e537432e7b7732bed8800b58efba365d5db0890b
valflrt <valflrt@pm.me>
2022-11-20 15:15:52 +0100

    Improve simulation.py

--- simulation.py
+++ simulation.py
@@ -1,25 +1,18 @@
-import turtle
+import turtle, argparse
 
-# Fonction qui remplace inverse l'ordre d'une liste et qui
-# inverse les 0 et les 1
+# Fonction qui inverse l'ordre d'une liste et qui remplace
+# les 0 par des 1 et les 1 par des 0
 def inverser_seq(seq):
-    inv = []
-    for nb in seq:
-        # remplace les zéros par des uns et inversement
-        inv.append(1 if nb == 0 else 0)
-    return inv[::-1] # [::-1] retourne le tableau
+    return [(1 if e == 0 else 0) for e in seq][::-1]
 
 # Fonction de récurrence pour obtenir les angles à un certain
 # rang n (similaire au suite définies par récurrence en
 # mathématiques)
 A0 = []
 def A(n):
-    if n == 0:
-        return A0
-    else:
-        return inverser_seq(A(n - 1)) + [0] + A(n - 1)
+    return inverser_seq(A(n - 1)) + [0] + A(n - 1) if n > 0 else A0
 
-def dessiner_sequence(n = 1, seg_len = 20, capture = False, show_angles = False):
+def dessiner_sequence(n = 1, seg_len = 20, show_angles = False, capture = False):
     if n < 1:
         print("Attention: impossible de tracer une figure avec moins de 1 pliage (n ne doit pas être plus petit que 1)")
         return
@@ -30,15 +23,13 @@ def dessiner_sequence(n = 1, seg_len = 20, capture = False, show_angles = False)
 
     if show_angles == True:
         print("angles:", angles)
-    print("nombre de pliages: ", n)
-    print("nombre d'angles:   ", nb_angles)
-    print("nombre de segments:", nb_segments)
+    print(n, "pliages,", nb_angles, "angles,", nb_segments, "segments")
 
     # obtient les angles de la figure au rang n - 1, pour
-    # pouvoir placer le centre de la figure au point (0, 0)
+    # pouvoir placer le "centre" de la figure au point (0, 0)
     # (la figure à un certain rang est simplement la figure
     # au rang n - 1 dessinée en partant du même point mais
-    # avec un décalage angulaire de 90°)
+    # avec un angle de 90°)
     prev_angles = A(n - 1)
 
     # cache la turtle
@@ -84,7 +75,7 @@ def dessiner_sequence(n = 1, seg_len = 20, capture = False, show_angles = False)
     if n > 2:
         print("taille quadrillage:", int((maximums[2] - maximums[0]) / seg_len), "x", int((maximums[3] - maximums[1]) / seg_len))
 
-    # sauvegarde une image si demandé
+    # sauvegarde une "image" si demandé
     if capture == True:
         screen = turtle.getscreen()
         screen.getcanvas().postscript(file="img.eps")
@@ -94,4 +85,15 @@ def dessiner_sequence(n = 1, seg_len = 20, capture = False, show_angles = False)
 
 # ne pas mettre un nombre trop grand pour n parce que sinon
 # le pc aime pas
-dessiner_sequence(n = 12, seg_len = 4, show_angles = False)
+dessiner_sequence(n = 11, seg_len = 5)
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument("n")
+# parser.add_argument("-s", "--segment_len", default = 20)
+# parser.add_argument("-a", "--show-angles", action="store_true", default = False)
+# parser.add_argument("-c", "--capture", action="store_true", default = False)
+
+# args = parser.parse_args()
+
+# dessiner_sequence(int(args.n), int(args.segment_len), bool(args.show_angles), bool(args.capture))
